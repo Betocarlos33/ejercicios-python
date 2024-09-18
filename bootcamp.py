32902ddce514e37e82f92fdef12dac612a15c601
Betocarlos33 <carlos.matteosguzman@gmail.com>
2024-09-17 20:16:32 -0600

    first commit

---
+++ bootcamp.py
@@ -0,0 +1,3 @@
+print ("Bienvenido al registro del Bootcamp")
+nombre = input("Por favor ingrese su nombre: ")
+print ("Gracias por su inscripcion:", nombre)
---
+++ pizza.py
@@ -0,0 +1,1 @@
+nombre = input("Por favor ingrese su nombre: ")
